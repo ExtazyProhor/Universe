fd9f34933fe35aa447c7fbb3921ea011508919ad
tima <prohor2568@gmail.com>
2025-09-02 17:19:36 +0300

    add Result, that can help with returning from functions values

---
+++ jawa/jocasta/jocasta-core/src/main/java/ru/prohor/universe/jocasta/core/collections/Result.java
@@ -0,0 +1,40 @@
+package ru.prohor.universe.jocasta.core.collections;
+
+import jakarta.annotation.Nonnull;
+
+import java.util.function.Function;
+
+public record Result<T>(
+        T result,
+        String error
+) {
+    public static <T> Result<T> of(T result) {
+        return new Result<>(result, null);
+    }
+
+    public static <T> Result<T> error(@Nonnull String message) {
+        return new Result<>(null, message);
+    }
+
+    public boolean isSuccess() {
+        return error == null;
+    }
+
+    public boolean isError() {
+        return !isSuccess();
+    }
+
+    public <U> Result<U> map(Function<T, U> remappingFunction) {
+        if (isError())
+            return new Result<>(null, error);
+        return new Result<>(remappingFunction.apply(result), null);
+    }
+
+    public <U> U mapOrElse(Function<T, U> remappingFunction, Function<String, U> errorMappingFunction) {
+        if (isSuccess())
+            return remappingFunction.apply(result);
+        return errorMappingFunction.apply(error);
+    }
+
+    // TODO methods from Opt<>
+}
