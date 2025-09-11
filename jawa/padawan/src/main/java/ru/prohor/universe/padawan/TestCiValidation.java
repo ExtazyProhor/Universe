7caf265e51bd15e9bd2e6871814f0e0f47e3df25
tima <prohor2568@gmail.com>
2025-09-11 13:13:37 +0300

    refactor test ci validation

--- jawa/padawan/src/main/java/ru/prohor/universe/padawan/TestCiValidation.java
+++ jawa/padawan/src/main/java/ru/prohor/universe/padawan/TestCiValidation.java
@@ -1,59 +1,47 @@
 package ru.prohor.universe.padawan;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import ru.prohor.universe.jocasta.core.collections.Opt;
-import ru.prohor.universe.jocasta.core.features.sneaky.Sneaky;
 
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 
 public class TestCiValidation {
-    public record Body(
-            Response response
+    public record Metadata(
+            String groupId,
+            String artifactId,
+            Versioning versioning
     ) {}
 
-    public record Response(
-            @JsonProperty("numFound")
-            int total,
-            List<Artifact> docs
+    public record Versioning(
+            List<String> versions,
+            String latest,
+            String release,
+            String lastUpdated
     ) {}
-
-    public record Artifact(
-            @JsonProperty("v")
-            String version,
-            long timestamp
-    ) {}
-
-    public static void main(String[] args) throws Exception {
-        String s = request("org.springframework.boot", "spring-boot-dependencies").get();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        Body body = mapper.readValue(s, Body.class);
-
-        body.response.docs.stream()
-                .sorted(Comparator.comparing(Artifact::timestamp))
-                .map(Artifact::version)
-                .forEach(System.out::println);
-    }
 
     private static final OkHttpClient client = new OkHttpClient();
 
-    public static Opt<String> request(String group, String artifact) throws IOException {
+    public static void main(String[] args) throws Exception {
+        Metadata metadata = request("org.springframework.boot", "spring-boot-dependencies");
+
+        metadata.versioning.versions.stream()
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+
+        System.out.println(metadata);
+    }
+
+    public static Metadata request(String group, String artifact) throws IOException {
+        String path = group.replace('.', '/') + "/" + artifact + "/maven-metadata.xml";
+
         HttpUrl url = new HttpUrl.Builder()
                 .scheme("https")
-                .host("search.maven.org")
-                .addPathSegments("solrsearch/select")
-                .addQueryParameter("q", "g:\"" + group + "\" AND a:\"" + artifact + "\"")
-                .addQueryParameter("core", "gav")
-                .addQueryParameter("rows", "200") // pagination
-                .addQueryParameter("wt", "json")
+                .host("repo1.maven.org")
+                .addPathSegments("maven2/" + path)
                 .build();
 
         Request request = new Request.Builder()
@@ -65,7 +53,10 @@ public class TestCiValidation {
             if (!response.isSuccessful()) {
                 throw new RuntimeException("Ошибка запроса: " + response);
             }
-            return Opt.ofNullable(response.body()).map(body -> Sneaky.wrap(body::string));
+
+            String xml = response.body().string();
+            XmlMapper mapper = new XmlMapper();
+            return mapper.readValue(xml, Metadata.class);
         }
     }
 }
