9a9434d6dee8b35228b4c78fb4b607daa9fc7001
tima <prohor2568@gmail.com>
2025-09-21 02:18:56 +0300

    add yandex test contest class

---
+++ jawa/padawan/src/main/java/ru/prohor/universe/padawan/tasks/yacontest/Contest_19036.java
@@ -0,0 +1,48 @@
+package ru.prohor.universe.padawan.tasks.yacontest;
+
+@ContestRound("Бэкенд — Пробный раунд")
+public class Contest_19036 {
+    public static void main(String[] args) {
+
+    }
+
+    @ContestTask("Камни и украшения")
+    private static void TaskA() {
+
+    }
+
+    @ContestTask("Будильники")
+    private static void TaskB() {
+
+    }
+
+    @ContestTask("Интересная игра")
+    private static void TaskC() {
+
+    }
+
+    @ContestTask("Поиск ломающего коммита")
+    private static void TaskD() {
+
+    }
+
+    @ContestTask("Скачивание ресурсов в дата-центре")
+    private static void TaskE() {
+
+    }
+
+    @ContestTask("Сложение чисел")
+    private static void TaskF() {
+
+    }
+
+    @ContestTask("SQL")
+    private static void TaskG() {
+
+    }
+
+    @ContestTask("Сервис валидации телефонных номеров")
+    private static void TaskH() {
+
+    }
+}
