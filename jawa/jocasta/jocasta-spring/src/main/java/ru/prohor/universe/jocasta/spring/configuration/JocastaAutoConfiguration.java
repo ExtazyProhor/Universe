dc0137bb28ec14e20b767d9c0145ee2ab53e7bc8
tima <prohor2568@gmail.com>
2025-09-22 23:47:44 +0300

    exclude mongo auto configuration

--- jawa/jocasta/jocasta-spring/src/main/java/ru/prohor/universe/jocasta/spring/configuration/JocastaAutoConfiguration.java
+++ jawa/jocasta/jocasta-spring/src/main/java/ru/prohor/universe/jocasta/spring/configuration/JocastaAutoConfiguration.java
@@ -3,7 +3,11 @@ package ru.prohor.universe.jocasta.spring.configuration;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +20,11 @@ import org.springframework.core.env.Environment;
         JpaRepositoriesAutoConfiguration.class,
         DataSourceAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class,
+
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class,
+        MongoReactiveAutoConfiguration.class,
+        MongoReactiveDataAutoConfiguration.class,
 })
 public class JocastaAutoConfiguration {
     private static final String NO_ENV_MESSAGE = """
