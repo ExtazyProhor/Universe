921b7cb042a7d81f8b376e0ba1cd03ee5d3dc1fc
tima <prohor2568@gmail.com>
2025-10-16 15:31:00 +0300

    add Paginator

---
+++ jawa/jocasta/jocasta-core/src/main/java/ru/prohor/universe/jocasta/core/collections/Paginator.java
@@ -0,0 +1,42 @@
+package ru.prohor.universe.jocasta.core.collections;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Paginator { // TODO move to custom List
+    private Paginator() {}
+
+    public static <T> List<T> paginate(List<T> list, long page, int size) {
+        check(page, size);
+        return unsafePaginate(list, page, size);
+    }
+
+    public static <T> Stream<T> paginate(Stream<T> stream, long page, int size) {
+        check(page, size);
+        return unsafePaginate(stream, page, size);
+    }
+
+    public static <T> List<T> paginateOrLastPage(List<T> list, long page, int size) {
+        check(page, size);
+        int length = list.size();
+        long skip = (long) page * size;
+        if (skip < length)
+            return unsafePaginate(list, page, size);
+        return unsafePaginate(list, (length - 1) / size, size);
+    }
+
+    private static <T> List<T> unsafePaginate(List<T> list, long page, int size) {
+        return unsafePaginate(list.stream(), page, size).toList();
+    }
+
+    private static <T> Stream<T> unsafePaginate(Stream<T> stream, long page, int size) {
+        return stream.skip(page * size).limit(size);
+    }
+
+    private static void check(long page, int size) {
+        if (page < 0)
+            throw new IllegalArgumentException("page must be greater than or equal 0");
+        if (size < 1)
+            throw new IllegalArgumentException("page size must be greater than or equal 1");
+    }
+}
