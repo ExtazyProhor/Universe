67130df2b0089a86556a172a1d26c4bc92c4e67e
tima <prohor2568@gmail.com>
2025-10-21 18:27:25 +0300

    add more favicons

--- jawa/jocasta/jocasta-spring-web/src/main/java/ru/prohor/universe/jocasta/springweb/controllers/FaviconController.java
+++ jawa/jocasta/jocasta-spring-web/src/main/java/ru/prohor/universe/jocasta/springweb/controllers/FaviconController.java
@@ -10,8 +10,13 @@ import java.nio.file.Path;
 
 @Controller
 public class FaviconController {
-    private static final String FAVICON_FILE = "favicon.ico";
-    private static final String FAVICON_PATH = "/" + FAVICON_FILE;
+    private static final String FAVICON_ICO = "favicon.ico";
+    private static final String FAVICON_SVG = "favicon.svg";
+    private static final String APPLE_TOUCH_ICON_PNG = "apple-touch-icon.png";
+    private static final String FAVICON_96_X_96_PNG = "favicon-96x96.png";
+    private static final String SITE_WEBMANIFEST = "site.webmanifest";
+    private static final String WEB_APP_MANIFEST_512_X_512_PNG = "web-app-manifest-512x512.png";
+    private static final String WEB_APP_MANIFEST_192_X_192_PNG = "web-app-manifest-192x192.png";
 
     private final StaticResourcesHandler staticResourcesHandler;
     private final Path faviconsDirectory;
@@ -24,8 +29,39 @@ public class FaviconController {
         this.faviconsDirectory = Path.of(pathToFaviconsDirectory);
     }
 
-    @GetMapping(FAVICON_PATH) // TODO иконки для других платформ
-    public ResponseEntity<FileSystemResource> favicon() {
-        return staticResourcesHandler.getResource(faviconsDirectory.resolve(FAVICON_FILE));
+    // TODO одна ручка, которая сама собирает файлы
+    @GetMapping("/" + FAVICON_ICO)
+    public ResponseEntity<FileSystemResource> faviconIco() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(FAVICON_ICO));
+    }
+
+    @GetMapping("/" + FAVICON_SVG)
+    public ResponseEntity<FileSystemResource> faviconSvg() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(FAVICON_SVG));
+    }
+
+    @GetMapping("/" + APPLE_TOUCH_ICON_PNG)
+    public ResponseEntity<FileSystemResource> appleTouch() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(APPLE_TOUCH_ICON_PNG));
+    }
+
+    @GetMapping("/" + FAVICON_96_X_96_PNG)
+    public ResponseEntity<FileSystemResource> favicon96x96() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(FAVICON_96_X_96_PNG));
+    }
+
+    @GetMapping("/" + SITE_WEBMANIFEST)
+    public ResponseEntity<FileSystemResource> siteWebmanifest() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(SITE_WEBMANIFEST));
+    }
+
+    @GetMapping("/" + WEB_APP_MANIFEST_512_X_512_PNG)
+    public ResponseEntity<FileSystemResource> manifest512x512() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(WEB_APP_MANIFEST_512_X_512_PNG));
+    }
+
+    @GetMapping("/" + WEB_APP_MANIFEST_192_X_192_PNG)
+    public ResponseEntity<FileSystemResource> manifest192x192() {
+        return staticResourcesHandler.getResource(faviconsDirectory.resolve(WEB_APP_MANIFEST_192_X_192_PNG));
     }
 }
