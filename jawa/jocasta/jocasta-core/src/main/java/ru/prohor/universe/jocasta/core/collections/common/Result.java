b5216303f4725e45e5579e8bac08272c2a62d5d4
tima <prohor2568@gmail.com>
2025-10-27 14:32:56 +0300

    add tryOr and ofNullable to Result

--- jawa/jocasta/jocasta-core/src/main/java/ru/prohor/universe/jocasta/core/collections/common/Result.java
+++ jawa/jocasta/jocasta-core/src/main/java/ru/prohor/universe/jocasta/core/collections/common/Result.java
@@ -1,6 +1,8 @@
 package ru.prohor.universe.jocasta.core.collections.common;
 
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import ru.prohor.universe.jocasta.core.features.sneaky.ThrowableSupplier;
 
 import java.util.function.Function;
 
@@ -8,8 +10,28 @@ public record Result<T>(
         T result,
         String error
 ) {
-    public static <T> Result<T> of(T result) {
+    public static <T> Result<T> of(@Nullable T result) {
         return new Result<>(result, null);
+    }
+
+    public static <T> Result<T> ofNullable(@Nullable T result, @Nonnull String message) {
+        return result == null ? new Result<>(null, message) : new Result<>(result, null);
+    }
+
+    public static <T> Result<T> tryOr(@Nonnull ThrowableSupplier<T> supplier, @Nonnull String message) {
+        try {
+            return new Result<>(supplier.get(), null);
+        } catch (Exception ignored) {
+            return new Result<>(null, message);
+        }
+    }
+
+    public static <T> Result<T> tryOr(@Nonnull ThrowableSupplier<T> supplier) {
+        try {
+            return new Result<>(supplier.get(), null);
+        } catch (Exception e) {
+            return new Result<>(null, e.getMessage());
+        }
     }
 
     public static <T> Result<T> error(@Nonnull String message) {
@@ -35,6 +57,4 @@ public record Result<T>(
             return remappingFunction.apply(result);
         return errorMappingFunction.apply(error);
     }
-
-    // TODO methods from Opt<>
 }
