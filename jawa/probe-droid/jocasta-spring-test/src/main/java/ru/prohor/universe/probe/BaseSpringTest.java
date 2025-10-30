56b4134c0eb46751961f3258457cad04f45a13c2
tima <prohor2568@gmail.com>
2025-10-30 16:08:48 +0300

    move properties to jocasta-spring-test

--- jawa/probe-droid/jocasta-spring-test/src/main/java/ru/prohor/universe/probe/BaseSpringTest.java
+++ jawa/probe-droid/jocasta-spring-test/src/main/java/ru/prohor/universe/probe/BaseSpringTest.java
@@ -3,6 +3,6 @@ package ru.prohor.universe.probe;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource("classpath:test.properties")
+@TestPropertySource("classpath:jocasta-spring-test.properties")
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public abstract class BaseSpringTest {}
