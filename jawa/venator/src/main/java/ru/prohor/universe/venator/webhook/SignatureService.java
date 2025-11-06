52bd8979a2aa0e8aa68bbe32770894ba215a08bd
tima <prohor2568@gmail.com>
2025-11-06 23:12:05 +0300

    add SignatureService for venator's webhooks

---
+++ jawa/venator/src/main/java/ru/prohor/universe/venator/webhook/SignatureService.java
@@ -0,0 +1,23 @@
+package ru.prohor.universe.venator.webhook;
+
+import org.apache.commons.codec.digest.HmacAlgorithms;
+import org.apache.commons.codec.digest.HmacUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignatureService {
+    private final String secret;
+
+    public SignatureService(@Value("${universe.venator.webhook.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    public boolean verifySignature(String signature, String body) {
+        if (signature == null)
+            return false;
+        String hmacHex = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret).hmacHex(body);
+        String expectedSignature = "sha256=" + hmacHex;
+        return signature.equals(expectedSignature);
+    }
+}
