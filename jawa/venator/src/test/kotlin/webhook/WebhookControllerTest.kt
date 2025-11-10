79fa4e462a69443bb697d8199572e6764b54a9df
tima <prohor2568@gmail.com>
2025-11-10 17:03:08 +0300

    add more tests on WebhookController

--- jawa/venator/src/test/kotlin/webhook/WebhookControllerTest.kt
+++ jawa/venator/src/test/kotlin/webhook/WebhookControllerTest.kt
@@ -17,9 +17,11 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.test.context.TestConstructor
 import ru.prohor.universe.venator.Venator
+import ru.prohor.universe.venator.webhook.model.ApiResponse
 import ru.prohor.universe.venator.webhook.model.WebhookPayload
 
 @SpringBootTest(
@@ -36,28 +38,118 @@ class WebhookControllerTest(
     fun testParseWebhookPayload() {
         val payload = getResource("/webhook-payload-mock.json")
         val headers = HttpHeaders()
-        val hmacUtils = HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret)
-        headers.add("X-Hub-Signature-256", "sha256=" + hmacUtils.hmacHex(payload))
-        headers.contentType = MediaType.APPLICATION_JSON
-        headers.accept = listOf(MediaType.APPLICATION_JSON)
+        addSignature(payload, headers)
 
-        val response = rest.exchange(
-            "/webhook/on_push",
-            HttpMethod.POST,
-            HttpEntity(payload, headers),
-            String::class.java
-        )
-
+        val response = doRequest(payload, headers)
         Assertions.assertEquals(HttpStatus.OK, response.statusCode)
 
         val expectedRestPayload = getResource("/webhook-rest-payload-mock.json")
-        val parsedPayload = DebugWebhookAction.Companion.payload
+        val parsedPayload = DebugWebhookAction.payload
         Assertions.assertNotNull(parsedPayload)
 
         JSONAssert.assertEquals(
             expectedRestPayload,
             ObjectMapper().writeValueAsString(parsedPayload),
             JSONCompareMode.NON_EXTENSIBLE
+        )
+    }
+
+    @Test
+    fun testWebhookWithoutSignature() {
+        val payload = getResource("/webhook-payload-mock.json")
+        val headers = HttpHeaders()
+
+        val response = doRequest(payload, headers)
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+    @Test
+    fun testWebhookWithIllegalSignature() {
+        val payload = getResource("/webhook-payload-mock.json")
+        val headers = HttpHeaders()
+        addSignature(payload, headers, "some_illegal_secret")
+
+        val response = doRequest(payload, headers)
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+    @Test
+    fun testWebhookWithIllegalIp() {
+        val payload = getResource("/webhook-payload-mock.json")
+        val headers = HttpHeaders()
+        addSignature(payload, headers)
+        headers.add("X-Real-IP", "123.123.123.123")
+
+        val response = doRequest(payload, headers)
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    }
+
+    @Test
+    fun testWebhookWithAnotherRepository() {
+        val payload = getResource("/webhook-payload-mock.json")
+            .replace(
+                "\"full_name\": \"ExtazyProhor/Universe\"",
+                "\"full_name\": \"ExtazyProhor/Universe2\""
+            )
+        val headers = HttpHeaders()
+        addSignature(payload, headers)
+
+        val response = doRequest(payload, headers)
+
+        Assertions.assertEquals(HttpStatus.OK, response.statusCode)
+        Assertions.assertNotNull(response.body)
+        JSONAssert.assertEquals(
+            ObjectMapper().writeValueAsString(ApiResponse.error("Unknown repository")),
+            response.body,
+            JSONCompareMode.NON_EXTENSIBLE
+        )
+    }
+
+    @Test
+    fun testWebhookWithAnotherBranch() {
+        val payload = getResource("/webhook-payload-mock.json")
+            .replace(
+                "\"ref\": \"refs/heads/main\"",
+                "\"ref\": \"refs/heads/some-branch\""
+            )
+        val headers = HttpHeaders()
+        addSignature(payload, headers)
+
+        val response = doRequest(payload, headers)
+
+        Assertions.assertEquals(HttpStatus.OK, response.statusCode)
+        Assertions.assertNotNull(response.body)
+        JSONAssert.assertEquals(
+            ObjectMapper().writeValueAsString(ApiResponse.success("Ignored, wrong branch")),
+            response.body,
+            JSONCompareMode.NON_EXTENSIBLE
+        )
+    }
+
+    @Test
+    fun testWebhookWithNullFields() {
+        val payload = getResource("/webhook-payload-mock.json")
+            .replace("\"full_name\": \"ExtazyProhor/Universe\",", "")
+            .replace("\"ref\": \"refs/heads/main\",", "\"ref\": null,")
+        val headers = HttpHeaders()
+        addSignature(payload, headers)
+
+        val response = doRequest(payload, headers)
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.statusCode)
+    }
+
+    private fun addSignature(payload: String, headers: HttpHeaders, secret: String? = null) {
+        val hmacUtils = HmacUtils(HmacAlgorithms.HMAC_SHA_256, secret ?: this.secret)
+        headers.add("X-Hub-Signature-256", "sha256=" + hmacUtils.hmacHex(payload))
+    }
+
+    private fun doRequest(payload: String, headers: HttpHeaders): ResponseEntity<String> {
+        headers.contentType = MediaType.APPLICATION_JSON
+        return rest.exchange(
+            "/webhook/on_push",
+            HttpMethod.POST,
+            HttpEntity(payload, headers),
+            String::class.java
         )
     }
 
