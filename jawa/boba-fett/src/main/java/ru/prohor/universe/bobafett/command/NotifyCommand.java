14a17a97dceafcd94d2de44e10fb8b66410e8fbe
tima <prohor2568@gmail.com>
2025-12-15 17:53:16 +0300

    add NotifyCommand and WaitNotifyMessage

---
+++ jawa/boba-fett/src/main/java/ru/prohor/universe/bobafett/command/NotifyCommand.java
@@ -0,0 +1,66 @@
+package ru.prohor.universe.bobafett.command;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.prohor.universe.bobafett.data.BobaFettRepositoryHelper;
+import ru.prohor.universe.bobafett.data.pojo.BobaFettUser;
+import ru.prohor.universe.bobafett.data.pojo.UserStatus;
+import ru.prohor.universe.bobafett.status.WaitNotifyMessage;
+import ru.prohor.universe.jocasta.core.collections.common.Opt;
+import ru.prohor.universe.jocasta.morphia.MongoRepository;
+import ru.prohor.universe.jocasta.morphia.MongoTransactionService;
+import ru.prohor.universe.jocasta.tgbots.api.FeedbackExecutor;
+import ru.prohor.universe.jocasta.tgbots.api.comand.CommandHandler;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class NotifyCommand implements CommandHandler {
+    private final Set<Long> adminChatsIds;
+    private final MongoRepository<BobaFettUser> usersRepository;
+    private final MongoTransactionService transactionService;
+    private final WaitNotifyMessage waitNotifyMessage;
+
+    public NotifyCommand(
+            @Value("${universe.boba-fett.admin-ids}") List<Long> adminChatsIds,
+            MongoRepository<BobaFettUser> usersRepository,
+            MongoTransactionService transactionService,
+            WaitNotifyMessage waitNotifyMessage
+    ) {
+        this.adminChatsIds = new HashSet<>(adminChatsIds);
+        this.usersRepository = usersRepository;
+        this.transactionService = transactionService;
+        this.waitNotifyMessage = waitNotifyMessage;
+    }
+
+    @Override
+    public String command() {
+        return "/notify";
+    }
+
+    @Override
+    public boolean handle(Message message, FeedbackExecutor feedbackExecutor) {
+        Chat chat = message.getChat();
+        long chatId = chat.getId();
+        if (!adminChatsIds.contains(chatId))
+            return false;
+
+        transactionService.withTransaction(tx -> {
+            MongoRepository<BobaFettUser> transactional = tx.wrap(usersRepository);
+            Opt<BobaFettUser> user = BobaFettRepositoryHelper.findByChatId(transactional, chatId);
+            if (user.isEmpty()) {
+                return;
+            }
+            transactional.save(user.get().toBuilder().status(Opt.of(new UserStatus(
+                    waitNotifyMessage.key(),
+                    Opt.empty()
+            ))).build());
+            feedbackExecutor.sendMessage(chatId, "На первой строчке chatId через запятую, все остальное - сообщение");
+        });
+        return false;
+    }
+}
---
+++ jawa/boba-fett/src/main/java/ru/prohor/universe/bobafett/status/WaitNotifyMessage.java
@@ -0,0 +1,56 @@
+package ru.prohor.universe.bobafett.status;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.prohor.universe.bobafett.command.CancelCommandHandler;
+import ru.prohor.universe.jocasta.tgbots.api.FeedbackExecutor;
+import ru.prohor.universe.jocasta.tgbots.api.status.StatusHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class WaitNotifyMessage implements StatusHandler<String> {
+    private final CancelCommandHandler cancelCommandHandler;
+
+    public WaitNotifyMessage(CancelCommandHandler cancelCommandHandler) {
+        this.cancelCommandHandler = cancelCommandHandler;
+    }
+
+    @Override
+    public String key() {
+        return "admin/wait-notify-message";
+    }
+
+    @Override
+    public boolean handle(Update update, FeedbackExecutor feedbackExecutor) {
+        if (!update.hasMessage() || !update.getMessage().hasText()) {
+            return true;
+        }
+        long chatId = update.getMessage().getChatId();
+        String message = update.getMessage().getText();
+        if (message.equals(cancelCommandHandler.key())) {
+            feedbackExecutor.sendMessage(chatId, "Отправка сообщения отменена");
+            return false;
+        }
+        int lineIndex = message.indexOf('\n');
+        if (lineIndex < 0) {
+            feedbackExecutor.sendMessage(chatId, "Неверный формат, должно быть как минимум 2 строки");
+            return false;
+        }
+        List<Long> chatIds = new ArrayList<>();
+        for (String part : message.substring(0, lineIndex).split(",")) {
+            try {
+                chatIds.add(Long.parseLong(part));
+            } catch (NumberFormatException nfe) {
+                feedbackExecutor.sendMessage(chatId, "Неверный формат chatId: \"" + part + "\"");
+                return false;
+            }
+        }
+        message = message.substring(lineIndex + 1);
+        for (Long chat : chatIds) {
+            feedbackExecutor.sendMessage(chat, message);
+        }
+        return false;
+    }
+}
