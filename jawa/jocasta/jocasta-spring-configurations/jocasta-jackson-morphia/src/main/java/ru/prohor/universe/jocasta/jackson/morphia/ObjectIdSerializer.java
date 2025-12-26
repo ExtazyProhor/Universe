99190a28c7e37571985003d7606cb81b821e9be3
tima <prohor2568@gmail.com>
2025-12-26 14:48:39 +0300

    add jocasta-jackson-morphia module

---
+++ jawa/jocasta/jocasta-spring-configurations/jocasta-jackson-morphia/src/main/java/ru/prohor/universe/jocasta/jackson/morphia/ObjectIdSerializer.java
@@ -0,0 +1,15 @@
+package ru.prohor.universe.jocasta.jackson.morphia;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+
+public class ObjectIdSerializer extends JsonSerializer<ObjectId> {
+    @Override
+    public void serialize(ObjectId value, JsonGenerator generator, SerializerProvider provider) throws IOException {
+        generator.writeString(value.toHexString());
+    }
+}
