e88a640019685ae9bd9523674d80d2e57cfe5166
tima <prohor2568@gmail.com>
2025-12-29 05:05:08 +0300

    remove probe-droid

--- jawa/probe-droid/jocasta-spring-test/src/main/java/ru/prohor/universe/probe/spring/BaseSpringTest.java
+++
@@ -1,8 +0,0 @@
-package ru.prohor.universe.probe.spring;
-
-import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.TestPropertySource;
-
-@TestPropertySource("classpath:jocasta-spring-test.properties")
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-public abstract class BaseSpringTest {}
