933fb2a33956f4177d7241c4c7a45c068c73e1f8
tima <prohor2568@gmail.com>
2026-01-19 13:44:00 +0300

    remove unused import

--- jawa/venator/src/main/kotlin/Venator.kt
+++ jawa/venator/src/main/kotlin/Venator.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import ru.prohor.universe.jocasta.spring.configuration.HolocronConfiguration
 import ru.prohor.universe.jocasta.spring.configuration.JocastaAutoConfiguration
-import ru.prohor.universe.jocasta.springweb.features.cache.CachingRequestBodyConfiguration
 
 @Configuration
 @ComponentScan
