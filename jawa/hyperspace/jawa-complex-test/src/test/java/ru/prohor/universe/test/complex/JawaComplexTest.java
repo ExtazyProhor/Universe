00699e161f24005821951a93cc2e933f2a754f7a
tima <prohor2568@gmail.com>
2026-01-25 23:16:10 +0300

    add jawa complex test for testing compilability of all product modules

---
+++ jawa/hyperspace/jawa-complex-test/src/test/java/ru/prohor/universe/test/complex/JawaComplexTest.java
@@ -0,0 +1,11 @@
+package ru.prohor.universe.test.complex;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JawaComplexTest {
+    @Test
+    public void testAllModulesCompiles() {
+        Assertions.assertTrue(true);
+    }
+}
