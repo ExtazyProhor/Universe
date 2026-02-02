3dfbd4e97e6dacc4e080382a7b19b59a207fb0c8
tima <prohor2568@gmail.com>
2026-02-02 10:46:55 +0300

    add legacy yahtzee url printing

--- jawa/sabacc/yahtzee-legacy/src/main/java/ru/prohor/universe/yahtzee/legacy/YahtzeeLegacyMain.java
+++ jawa/sabacc/yahtzee-legacy/src/main/java/ru/prohor/universe/yahtzee/legacy/YahtzeeLegacyMain.java
@@ -1,9 +1,11 @@
 package ru.prohor.universe.yahtzee.legacy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import ru.prohor.universe.jocasta.spring.UniverseEnvironment;
 import ru.prohor.universe.jocasta.spring.configuration.HolocronConfiguration;
 import ru.prohor.universe.jocasta.spring.configuration.JocastaAutoConfiguration;
 import ru.prohor.universe.jocasta.springweb.configuration.FaviconControllerConfiguration;
@@ -24,6 +26,12 @@ import ru.prohor.universe.jocasta.springweb.configuration.StaticResourcesHandler
 })
 public class YahtzeeLegacyMain {
     public static void main(String[] args) {
-        SpringApplication.run(YahtzeeLegacyMain.class, args);
+        ApplicationContext context = SpringApplication.run(YahtzeeLegacyMain.class, args);
+        printUrl(context.getBean(UniverseEnvironment.class));
+    }
+
+    private static void printUrl(UniverseEnvironment environment) {
+        if (environment == UniverseEnvironment.LOCAL)
+            System.out.println("http://localhost:7003");
     }
 }
