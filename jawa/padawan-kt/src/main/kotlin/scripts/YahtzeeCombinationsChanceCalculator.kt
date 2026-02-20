e9b5383f43e7a069560a1d19073dcacbc03acd6c
tima <prohor2568@gmail.com>
2026-02-20 21:36:05 +0300

    add pair, three and four of kind combinations

--- jawa/padawan-kt/src/main/kotlin/scripts/YahtzeeCombinationsChanceCalculator.kt
+++ jawa/padawan-kt/src/main/kotlin/scripts/YahtzeeCombinationsChanceCalculator.kt
@@ -1,22 +1,94 @@
 package ru.prohor.universe.kt.padawan.scripts
 
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    // time: 78 ms!
-    // time: 231 ms!
-    // time: 132100 ms!
-    val time = measureTimeMillis {
-        println(ShortStreetSolver.calculate()) // 0.6025279139347071 / 0.6025279139347077
-        println(LongStreetSolver.calculate()) // 0.2491031974269013 / 0.24910319742690157
-        println(FullHouseSolver.calculate()) // 0.3588891746684954 / 0.3588891746684957
-        println(YahtzeeSolver.calculate()) // 0.04602864252569886 / 0.04602864252569899
+    test()
+    val solvers = listOf(
+        PairSolver,
+        ThreeOfKindSolver,
+        FourOfKindSolver,
+        FullHouseSolver,
+        ShortStreetSolver,
+        LongStreetSolver,
+        YahtzeeSolver
+    )
+    for (solver in solvers) {
+        var p: Double
+        val time = measureTimeMillis {
+            p = solver.calculate() * 100
+        }
+        println(
+            "${solver::class.simpleName?.removeSuffix("Solver")}: time = $time ms, p = $p %"
+        )
     }
-    println("time: $time ms!")
 }
 
-object ShortStreetSolver : ComplexCombinationSolver() {
+fun test() {
+    compare(0.3588891746684954, FullHouseSolver.calculate())
+    compare(0.6025279139347071, ShortStreetSolver.calculate())
+    compare(0.2491031974269013, LongStreetSolver.calculate())
+    compare(0.04602864252569886, YahtzeeSolver.calculate())
+}
+
+fun compare(expected: Double, actual: Double) {
+    if (abs(expected - actual) >= 1e-13) {
+        throw RuntimeException("Illegal value: expected=$expected, actual=$actual")
+    }
+}
+
+object PairSolver : SameDiceCombinationSolver() {
+    override val count = 2
+}
+
+object ThreeOfKindSolver : SameDiceCombinationSolver() {
+    override val count = 3
+}
+
+object FourOfKindSolver : SameDiceCombinationSolver() {
+    override val count = 4
+}
+
+object FullHouseSolver : CombinationSolver() {
+    override fun List<Int>.isValid(): Boolean {
+        val possibleCounts = hashSetOf(2, 3, 5)
+
+        val map = groupingBy { it }.eachCount()
+        if (map.size > 2)
+            return false
+        return possibleCounts.contains(map.values.first())
+    }
+
+    override fun List<Int>.holdDiceFor(): List<Int> {
+        val gist = groupingBy { it }
+            .eachCount()
+            .map { DiceCount(it.key, it.value) }
+            .sortedBy { it.count }
+            .reversed()
+        if (gist.first().count == 4) {
+            return List(4) { gist.first().value }
+        }
+        if (gist.first().count == 3) {
+            return List(3) { gist.first().value } + listOf(gist[1].value)
+        }
+        if (gist.first().count == 1) {
+            return subList(0, 2)
+        }
+        if (gist[1].count == 2) {
+            return List(2) { gist.first().value } + List(2) { gist[1].value }
+        }
+        return List(2) { gist.first().value } + listOf(gist[1].value)
+    }
+
+    private data class DiceCount(
+        val value: Int,
+        val count: Int
+    )
+}
+
+object ShortStreetSolver : CombinationSolver() {
     override fun List<Int>.isValid(): Boolean {
         val set = toHashSet()
         if (!set.containsAll(setOf(3, 4))) {
@@ -66,7 +138,7 @@ object ShortStreetSolver : ComplexCombinationSolver() {
     }
 }
 
-object LongStreetSolver : ComplexCombinationSolver() {
+object LongStreetSolver : CombinationSolver() {
     override fun List<Int>.isValid(): Boolean {
         val set = toHashSet()
         if (!set.containsAll(setOf(2, 3, 4, 5))) {
@@ -94,46 +166,18 @@ object LongStreetSolver : ComplexCombinationSolver() {
     }
 }
 
-object FullHouseSolver : ComplexCombinationSolver() {
-    override fun List<Int>.isValid(): Boolean {
-        val possibleCounts = hashSetOf(2, 3, 5)
-
-        val map = groupingBy { it }.eachCount()
-        if (map.size > 2)
-            return false
-        return possibleCounts.contains(map.values.first())
-    }
-
-    override fun List<Int>.holdDiceFor(): List<Int> {
-        val gist = groupingBy { it }
-            .eachCount()
-            .map { DiceCount(it.key, it.value) }
-            .sortedBy { it.count }
-            .reversed()
-        if (gist.first().count == 4) {
-            return List(4) { gist.first().value }
-        }
-        if (gist.first().count == 3) {
-            return List(3) { gist.first().value } + listOf(gist[1].value)
-        }
-        if (gist.first().count == 1) {
-            return subList(0, 2)
-        }
-        if (gist[1].count == 2) {
-            return List(2) { gist.first().value } + List(2) { gist[1].value }
-        }
-        return List(2) { gist.first().value } + listOf(gist[1].value)
-    }
-
-    private data class DiceCount(
-        val value: Int,
-        val count: Int
-    )
+object YahtzeeSolver : SameDiceCombinationSolver() {
+    override val count = 5
 }
 
-object YahtzeeSolver : ComplexCombinationSolver() {
+abstract class SameDiceCombinationSolver : CombinationSolver() {
+    abstract val count: Int
+
     override fun List<Int>.isValid(): Boolean {
-        return toSet().size == 1
+        return groupingBy { it }
+            .eachCount()
+            .maxBy { it.value }
+            .let { it.value >= count }
     }
 
     override fun List<Int>.holdDiceFor(): List<Int> {
@@ -144,7 +188,7 @@ object YahtzeeSolver : ComplexCombinationSolver() {
     }
 }
 
-abstract class ComplexCombinationSolver {
+abstract class CombinationSolver {
     private val cache = HashMap<State, Double>()
 
     abstract fun List<Int>.isValid(): Boolean
@@ -152,32 +196,37 @@ abstract class ComplexCombinationSolver {
     abstract fun List<Int>.holdDiceFor(): List<Int>
 
     fun calculate(): Double {
-        fun calculateInternal(
-            combination: List<Int>? = null,
-            rerollAttempt: Int = 0
-        ): Double {
-            val state = State(combination, rerollAttempt)
-            cache[state]?.let { return it }
-            if (combination?.isValid() == true) {
-                return 1.0
-            }
-            if (rerollAttempt == 3) {
-                return 0.0
-            }
-
-            val heldDice = combination?.holdDiceFor() ?: emptyList()
-            val diceRerolled = 5 - heldDice.size
-            val newCombinations = generatedCombinations[diceRerolled]
-            val nextRerollAttempt = rerollAttempt + 1
-            return newCombinations.sumOf { roll ->
-                val probability = roll.weight.toDouble() / powersOfSix[diceRerolled]
-                probability * calculateInternal(
-                    combination = heldDice + roll.values,
-                    rerollAttempt = nextRerollAttempt
-                )
-            }.also { cache[state] = it }
-        }
         return calculateInternal()
+    }
+
+    fun calculate(combination: List<Int>, roll: Int): Double {
+        return calculateInternal(combination, roll)
+    }
+
+    fun calculateInternal(
+        combination: List<Int>? = null,
+        rerollAttempt: Int = 0
+    ): Double {
+        val state = State(combination, rerollAttempt)
+        cache[state]?.let { return it }
+        if (combination?.isValid() == true) {
+            return 1.0
+        }
+        if (rerollAttempt == 3) {
+            return 0.0
+        }
+
+        val heldDice = combination?.holdDiceFor() ?: emptyList()
+        val diceRerolled = 5 - heldDice.size
+        val newCombinations = generatedCombinations[diceRerolled]
+        val nextRerollAttempt = rerollAttempt + 1
+        return newCombinations.sumOf { roll ->
+            val probability = roll.weight.toDouble() / powersOfSix[diceRerolled]
+            probability * calculateInternal(
+                combination = heldDice + roll.values,
+                rerollAttempt = nextRerollAttempt
+            )
+        }.also { cache[state] = it }
     }
 
     private data class State(
@@ -204,13 +253,11 @@ private data class Roll(
 )
 
 private fun generateCombinations(length: Int): Sequence<List<Int>> {
-    val range = 1..6
-
     if (length <= 0) {
         return sequenceOf(emptyList())
     }
 
-    return range.asSequence().flatMap { value ->
+    return (1..6).asSequence().flatMap { value ->
         generateCombinations(length - 1).map { list ->
             listOf(value) + list
         }
