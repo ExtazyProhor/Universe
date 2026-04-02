be733cfcf746f93d2f4eb35160f96c60b43284b5
tima <prohor2568@gmail.com>
2026-04-02 23:49:52 +0300

    add MusicViewer

--- jawa/padawan/src/main/java/ru/prohor/universe/padawan/PadawanSpringMain.java
+++ jawa/padawan/src/main/java/ru/prohor/universe/padawan/PadawanSpringMain.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ru.prohor.universe.chopper.client.ChopperClientConfig;
 import ru.prohor.universe.jocasta.spring.configuration.HolocronConfiguration;
 import ru.prohor.universe.jocasta.spring.configuration.JocastaAutoConfiguration;
+import ru.prohor.universe.jocasta.springweb.configuration.StaticResourcesHandlerConfiguration;
 
 @Configuration
 @ComponentScan
 @Import({
         JocastaAutoConfiguration.class,
         HolocronConfiguration.class,
-        ChopperClientConfig.class,
+        StaticResourcesHandlerConfiguration.class,
 })
 public class PadawanSpringMain {
     public static void main(String[] args) {
