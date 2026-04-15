1448baa57bc993198a6061db63007443bc247f7b
tima <prohor2568@gmail.com>
2026-04-15 22:31:03 +0300

    fix JacksonConfiguration

--- jawa/jocasta/jocasta-spring/src/main/java/ru/prohor/universe/jocasta/spring/configuration/JacksonConfiguration.java
+++ jawa/jocasta/jocasta-spring/src/main/java/ru/prohor/universe/jocasta/spring/configuration/JacksonConfiguration.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,8 +18,6 @@ public class JacksonConfiguration {
     public ObjectMapper objectMapper(List<Module> modules) {
         return new ObjectMapper()
                 .registerModules(modules)
-                .registerModules(new JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .setDefaultPropertyInclusion(
                         JsonInclude.Value.construct(JsonInclude.Include.NON_NULL, JsonInclude.Include.NON_NULL)
