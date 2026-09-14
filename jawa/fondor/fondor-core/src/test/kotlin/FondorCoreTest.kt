261b7f4d9b50dca0e3a129abab1a3520c27e6d68
tima <prohor2568@gmail.com>
2026-09-14 04:54:45 +0300

    add fondor module for frontend mock-servers

---
+++ jawa/fondor/fondor-core/src/test/kotlin/FondorCoreTest.kt
@@ -0,0 +1,11 @@
+package ru.prohor.universe.fondor.core
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class FondorCoreTest {
+    @Test
+    fun testModuleBuilds() {
+        assertTrue(true)
+    }
+}
---
+++ jawa/fondor/fondor-scarif/src/test/kotlin/FondorScarifTest.kt
@@ -0,0 +1,11 @@
+package ru.prohor.universe.fondor.scarif
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class FondorScarifTest {
+    @Test
+    fun testModuleBuilds() {
+        assertTrue(true)
+    }
+}
